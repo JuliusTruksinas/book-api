3d7b2f376daff4d4c51d43d88b8602c31987884d
Julius Trukšinas <124052041+JuliusTruksinas@users.noreply.github.com>
2024-09-25 12:23:41 +0300

    new: added ability to get all of the reviews of a book (#19)

--- src/main/java/com/truksinas/bookApi/repositories/ReviewRepository.java
+++ src/main/java/com/truksinas/bookApi/repositories/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.truksinas.bookApi.repositories;
 
 import com.truksinas.bookApi.entities.ReviewEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -9,4 +11,5 @@ import org.springframework.data.repository.query.Param;
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Integer>, JpaSpecificationExecutor<ReviewEntity> {
     @Query("SELECT AVG(r.stars) FROM ReviewEntity r WHERE r.book.id = :bookId")
     Double findRatingByBookId(@Param("bookId") Integer bookId);
+    Page<ReviewEntity> findByBookId(Integer bookId, Pageable pageable);
 }
