55e13f82172703d470333a4c1a1a96974b910c0c
Julius Truksinas <julius@ababa.tech>
2024-09-25 03:10:02 +0300

    new: added constraints to all dtos, enabled validation for book controller

--- src/main/java/com/truksinas/bookApi/dtos/ReviewDto.java
+++ src/main/java/com/truksinas/bookApi/dtos/ReviewDto.java
@@ -1,11 +1,24 @@
 package com.truksinas.bookApi.dtos;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ReviewDto {
     private int id;
+
+    @NotBlank(message = "Title is mandatory")
+    @Size(max = 100, message = "Title must be less than 100 characters")
     private String title;
+
+    @NotBlank(message = "Content is mandatory")
+    @Size(max = 1000, message = "Content must be less than 1000 characters")
     private String content;
+
+    @Min(value = 1, message = "Stars must be at least 1")
+    @Max(value = 5, message = "Stars must be at most 5")
     private int stars;
 }
